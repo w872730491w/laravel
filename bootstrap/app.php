2d48c94dba791596a2faa7a95be41af48aa03c2f
wwy <872730491@qq.com>
2025-04-12 11:28:42 +0000

    添加 Laravel Telescope 依赖并更新相关配置，删除不再使用的请求类和 ESLint 配置文件，调整应用服务提供者以在本地和测试环境中注册 Telescope 服务提供者。

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,15 +5,22 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets;
+use Illuminate\Support\Facades\Route;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
+        using: function () {
+            Route::middleware('web')
+                ->prefix('admin')
+                ->name('admin.')
+                ->group(base_path('routes/admin.php'));
+        }
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->encryptCookies(except: []);
+        // $middleware->encryptCookies(except: []);
 
         $middleware->web(append: [
             HandleInertiaRequests::class,
